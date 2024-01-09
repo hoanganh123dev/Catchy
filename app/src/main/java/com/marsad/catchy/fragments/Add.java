ed407f590916f708f1279bb98694c231ea8da8be
Lê Xuân Ngọ <90897392+LeengoJ@users.noreply.github.com>
2024-01-09 12:34:04 +0700

    update

--- app/src/main/java/com/marsad/catchy/fragments/Add.java
+++ app/src/main/java/com/marsad/catchy/fragments/Add.java
@@ -8,9 +8,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -37,6 +39,7 @@ import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 import com.marsad.catchy.R;
+import com.marsad.catchy.verify.verify_decription;
 import com.marsad.catchy.adapter.GalleryAdapter;
 import com.marsad.catchy.model.GalleryImages;
 import com.theartofdev.edmodo.cropper.CropImage;
@@ -62,6 +65,10 @@ public class Add extends Fragment {
     private List<GalleryImages> list;
     private GalleryAdapter adapter;
     private FirebaseUser user;
+    private Button addbtn;
+
+    private boolean check;
+    private  String[] offensiveWords = {"buồi","buoi","dau buoi","daubuoi","caidaubuoi","nhucaidaubuoi","dau boi","bòi","dauboi","caidauboi","đầu bòy","đầu bùi","dau boy","dauboy","caidauboy","b","cặc","cak","kak","kac","cac","concak","nungcak","bucak","caiconcac","caiconcak","cu","cặk","cak","dái","giái","zái","kiu","cứt","cuccut","cutcut","cứk","cuk","cười ỉa","cười ẻ","đéo","đếch","đếk","dek","đết","đệt","đách","dech","d'","deo","d'","đel","đél","del","dell ngửi","dell ngui","dell chịu","dell chiu","dell hiểu","dell hieu","dellhieukieugi","dell nói","dell noi","dellnoinhieu","dell biết","dell biet","dell nghe","dell ăn","dell an","dell được","dell duoc","dell làm","dell lam","dell đi","dell di","dell chạy","dell chay","deohieukieugi","địt","đm","dm","đmm","dmm","đmmm","dmmm","đmmmm","dmmmm","đmmmmm","dmmmmm","đcm","dcm","đcmm","dcmm","đcmmm","dcmmm","đcmmmm","dcmmmm","đệch","đệt","dit","dis","diz","đjt","djt","địt mẹ","địt mịe","địt má","địt mía","địt ba","địt bà","địt cha","địt con","địt bố","địt cụ","dis me","disme","dismje","dismia","dis mia","dis mie","đis mía","đis mía","ditmemayconcho","ditmemay","ditmethangoccho","ditmeconcho","dmconcho","dmcs","ditmecondi","ditmecondicho","đụ","đụ mẹ","đụ mía","đụ mía","đụ má","đụ cha","đụ bà","đú cha","đú con mẹ","đú má","đú mẹ","đù cha","đù má","đù mẹ","đù mía","đù mía","đù mía","đù mía","đù mía","đù mía","đìu","đờ mờ","đê mờ","đờ ma ma","đờ mama","đê mama","đề mama","đê ma ma","đề ma ma","dou","doma","duoma","dou má","duo má","dou ma","đou má","đìu má","á đù","á đìu","đậu mẹ","đậu má","đĩ","di~","đuỹ","điếm","cđĩ","cdi~","đilol","điloz","đilon","diloz","dilol","dilon","condi","condi~","dime","di me","dimemay","condime","condimay","condimemay","con di cho","con di cho'","condicho","bitch","biz","bít chi","con bích","con bic","con bíc","con bít","phò","4","lồn","l`","loz","lìn","nulo","ml","matlon","cailon","matlol","matloz","thml","thangmatlon","thangml","đỗn lì","tml","thml","diml","dml","hãm","xàm lol","xam lol","xạo lol","xao lol","con lol","ăn lol","an lol","mát lol","mat lol","cái lol","cai lol","lòi lol","loi lol","ham lol","củ lol","cu lol","ngu lol","tuổi lol","tuoi lol","mõm lol","mồm lol","mom lol","như lol","nhu lol","nứng lol","nung lol","nug lol","nuglol","rảnh lol","ranh lol","đách lol","dach lol","mu lol","banh lol","tét lol","tet lol","vạch lol","vach lol","cào lol","cao lol","tung lol","mặt lol","mát lol","mat lol","xàm lon","xam lon","xạo lon","xao lon","con lon","ăn lon","an lon","mát lon","mat lon","cái lon","cai lon","lòi lon","loi lon","ham lon","củ lon","cu lon","ngu lon","tuổi lon","tuoi lon","mõm lon","mồm lon","mom lon","như lon","nhu lon","nứng lon","nung lon","nug lon","nuglon","rảnh lon","ranh lon","đách lon","dach lon","mu lon","banh lon","tét lon","tet lon","vạch lon","vach lon","cào lon","cao lon","tung lon","mặt lon","mát lon","mat lon","cái lờ","cl","clgt","cờ lờ gờ tờ","cái lề gì thốn","đốn cửa lòng","sml","sapmatlol","sapmatlon","sapmatloz","sấp mặt","sap mat","vlon","vloz","vlol","vailon","vai lon","vai lol","vailol","nốn lừng","vcl","vl","vleu","chịch","chich","vãi","v~","đụ","nứng","nug","đút đít","chổng mông","banh háng","xéo háng","xhct","xephinh","la liếm","đổ vỏ","xoạc","xoac","chich choac","húp sò","fuck","fck","đụ","bỏ bú","buscu","ngu","óc chó","occho","lao cho","láo chó","bố láo","chó má","cờ hó","sảng","thằng chó","thang cho'","thang cho","chó điên","thằng điên","thang dien","đồ điên","sủa bậy","sủa tiếp","sủa đi","sủa càn","mẹ bà","mẹ cha mày","me cha may","mẹ cha anh","mẹ cha nhà anh","mẹ cha nhà mày","me cha nha may","mả cha mày","mả cha nhà mày","ma cha may","ma cha nha may","mả mẹ","mả cha","kệ mẹ","kệ mịe","kệ mịa","kệ mje","kệ mja","ke me","ke mie","ke mia","ke mja","ke mje","bỏ mẹ","bỏ mịa","bỏ mịe","bỏ mja","bỏ mje","bo me","bo mia","bo mie","bo mje","bo mja","chetme","chet me","chết mẹ","chết mịa","chết mja","chết mịe","chết mie","chet mia","chet mie","chet mja","chet mje","thấy mẹ","thấy mịe","thấy mịa","thay me","thay mie","thay mia","tổ cha","bà cha mày","cmn","cmnl","tiên sư nhà mày","tiên sư bố","tổ sư"};
 
     public Add() {
         // Required empty public constructor
@@ -91,40 +98,48 @@ public class Add extends Fragment {
         clickListener();
 
     }
-
     private void clickListener() {
+//        if(verify_decription.containsOffensiveWords(descET.toString(),0.3,offensiveWords))
+//            check = true;
+//        else
+//            check = false;
+//        if(check=true){
+            adapter.SendImage(picUri -> CropImage.activity(picUri)
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .setAspectRatio(4, 3)
+                    .start(getContext(), Add.this));
 
-        adapter.SendImage(picUri -> CropImage.activity(picUri)
-                .setGuidelines(CropImageView.Guidelines.ON)
-                .setAspectRatio(4, 3)
-                .start(getContext(), Add.this));
+            nextBtn.setOnClickListener(v -> {
+                if(0==0) {
+                    FirebaseStorage storage = FirebaseStorage.getInstance();
+                    final StorageReference storageReference = storage.getReference().child("Post Images/" + System.currentTimeMillis());
 
-        nextBtn.setOnClickListener(v -> {
+                    dialog.show();
 
-            FirebaseStorage storage = FirebaseStorage.getInstance();
-            final StorageReference storageReference = storage.getReference().child("Post Images/" + System.currentTimeMillis());
+                    storageReference.putFile(imageUri)
+                            .addOnCompleteListener(task -> {
+                                if (task.isSuccessful()) {
 
-            dialog.show();
+                                    storageReference.getDownloadUrl().addOnSuccessListener(uri -> uploadData(uri.toString()));
 
-            storageReference.putFile(imageUri)
-                    .addOnCompleteListener(task -> {
-                        if (task.isSuccessful()) {
-
-                            storageReference.getDownloadUrl().addOnSuccessListener(uri -> uploadData(uri.toString()));
-
-                        } else {
-                            dialog.dismiss();
-                            Toast.makeText(getContext(), "Failed to upload post", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-
-        });
-
-    }
+                                } else {
+                                    dialog.dismiss();
+                                    Toast.makeText(getContext(), "Failed to upload post", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                }else
+                    Toast.makeText(getContext(), "bài đăng chứa nộ dụng nhạy cảm", Toast.LENGTH_SHORT).show();
+            });
+        }
+//    }
 
     private void uploadData(String imageURL) {
 
-
+        if(verify_decription.containsOffensiveWords(descET.toString(),0.3,offensiveWords))
+            check = false;
+        else
+            check = true;
+        if(check==true){
         CollectionReference reference = FirebaseFirestore.getInstance().collection("Users")
                 .document(user.getUid()).collection("Post Images");
 
@@ -161,6 +176,7 @@ public class Add extends Fragment {
                 });
 
     }
+    }
 
     private void init(View view) {
 
@@ -169,7 +185,18 @@ public class Add extends Fragment {
         recyclerView = view.findViewById(R.id.recyclerView);
         backBtn = view.findViewById(R.id.backBtn);
         nextBtn = view.findViewById(R.id.nextBtn);
+        addbtn = view.findViewById(R.id.verify_btn);
 
+        addbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("TAG", "clickCheck: ");
+                if(verify_decription.containsOffensiveWords(descET.getText().toString(),0.8,offensiveWords)==false)
+                    Toast.makeText(getContext(), "nội dung hợp lệ", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getContext(), "bài đăng chứa nộ dụng nhạy cảm", Toast.LENGTH_SHORT).show();
+            }
+        });
         user = FirebaseAuth.getInstance().getCurrentUser();
 
         dialog = new Dialog(getContext());
@@ -245,10 +272,20 @@ public class Add extends Fragment {
 
                 imageView.setVisibility(View.VISIBLE);
                 nextBtn.setVisibility(View.VISIBLE);
-
             }
-
         }
 
     }
+
+
+//    public void clickCheck(View view) {
+//        Log.d("TAG", "clickCheck: ");
+//        if(verify_decription.containsOffensiveWords(descET.toString(),0.3,offensiveWords))
+//            Toast.makeText(getContext(), "nội dung hợp lệ", Toast.LENGTH_SHORT).show();
+//        else
+//            Toast.makeText(getContext(), "bài đăng chứa nộ dụng nhạy cảm", Toast.LENGTH_SHORT).show();
+//    }
+
+
+
 }
---
+++ app/src/main/java/com/marsad/catchy/verify/verify_decription.java
@@ -0,0 +1,126 @@
+package com.marsad.catchy.verify;
+
+public class verify_decription {
+    public static String replaceCharacterReplacements(String text) {
+        return text.replace("€", "e")
+                .replace("€", "e")
+                .replace("@", "a")
+                .replace("!", "i")
+                .replace("1", "i")
+                .replace("3", "e")
+                .replace("4", "a")
+                .replace("?", "i")
+                .replace("#", "a")
+                .replace("0", "o")
+                .replace("ß", "s")
+                .replaceAll("[^A-Za-z0-9]", " ");
+    }
+
+    public static boolean containsOffensiveWords(String text, double sensitivity, String[] offensiveWords) {
+        text = text.toLowerCase();
+        text = replaceCharacterReplacements(text);
+
+        text = text.replace(".", " ").replace(",", " ");
+
+        String[] words = text.split("\\s+");
+
+        for (int i = 0; i < words.length; i++) {
+            String word = removeDuplicates(words[i]);
+            if (offensiveWordsContains(word, sensitivity, offensiveWords)) return true;
+
+//            for (int j = i + 1; j < words.length; j++) {
+//                word += removeDuplicates(words[j]);
+//                if (offensiveWordsContains(word, sensitivity, offensiveWords)) return true;
+//            }
+        }
+
+        return false;
+    }
+
+    public static boolean offensiveWordsContains(String word, double sensitivity, String[] offensiveWords) {
+        for (String offensiveWord : offensiveWords) {
+            if (word.equals(offensiveWord)) return true;
+            if (word.contains(offensiveWord)) return true;
+            double offensive = findSimilarity(word, offensiveWord);
+            if (offensive > sensitivity) return true;
+        }
+        return false;
+    }
+
+    public  double getOffensiveWordPercentage(String text, String[] offensiveWords) {
+        text = text.toLowerCase();
+        text = replaceCharacterReplacements(text);
+        double percentage = 0.0;
+        for (String word : text.split(" ")) {
+            for (String offensiveWord : offensiveWords) {
+                double offensive = findSimilarity(word, offensiveWord);
+                percentage = percentage + offensive;
+            }
+        }
+        percentage = percentage / text.length();
+        return percentage;
+    }
+
+    public static double findSimilarity(String comparedWord, String word) {
+        if (comparedWord.equalsIgnoreCase(word)) return 1.0;
+        double maxLength = Double.max(comparedWord.length(), word.length());
+        if (maxLength > 0) return (maxLength - getLevenshteinDistance(comparedWord, word)) / maxLength;
+        return 1.0;
+    }
+
+    public static String replaceOffensiveWords(String message, String replacementBadWord, String replacementInvalidChar, double sensitivity, String... wordList) {
+        String filteredMessage = message;
+
+        filteredMessage = filteredMessage.replaceAll("[^a-zA-Z0-9?!%&/=:;öäüÖÄÜß\"$€´`'@(){}\\-_,.#*\\s]", replacementInvalidChar);
+
+        for (String word : filteredMessage.split(" ")) {
+            for (String offensiveWord : wordList) {
+                double offensive = findSimilarity(word.toLowerCase(), offensiveWord);
+                if (word.toLowerCase().equals(offensiveWord) || offensive > sensitivity) {
+                    String asterisks = new String(new char[word.length()]).replace("\0", replacementBadWord);
+                    filteredMessage = filteredMessage.replaceAll(word, asterisks);
+                }
+            }
+        }
+        return filteredMessage;
+    }
+
+
+    private static int getLevenshteinDistance(String x, String y) {
+        int m = x.length();
+        int n = y.length();
+        int[][] T = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            T[i][0] = i;
+        }
+        for (int j = 1; j <= n; j++) {
+            T[0][j] = j;
+        }
+        int cost;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                cost = x.charAt(i - 1) == y.charAt(j - 1) ? 0: 1;
+                T[i][j] = Integer.min(Integer.min(T[i - 1][j] + 1, T[i][j - 1] + 1),
+                        T[i - 1][j - 1] + cost);
+            }
+        }
+        return T[m][n];
+    }
+
+    private static String removeDuplicates(String text) {
+        if (text == null) {
+            return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        char prev = 0;
+        for (char c : text.toCharArray()) {
+            if (c != ' ' || prev != ' ') {
+                if (c != prev) {
+                    sb.append(c);
+                    prev = c;
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
